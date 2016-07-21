26426c22755658bdc56fff322a3d03cf087ed506
sarabhesh.k <sarabheswara.k-ext@medseek.com>
2016-07-21 16:50:59 +0530

    changed home control

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to demo ASP.NET MVCs!";
+            ViewBag.Message = "Welcome to demo ASP.NET MVC!";
 
             return View();
         }
