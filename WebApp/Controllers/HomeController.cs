cb00ecd516bc1a8f2e495a327ad8d3f78af1db5f
sarabhesh.k <sarabheswara.k-ext@medseek.com>
2016-07-21 15:38:34 +0530

    added ignore file

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to demo ASP.NET MVCs!";
 
             return View();
         }
